29a1060d1f4d2ab58852ad748c3528c8e0b2d8bb
md.golam morshed <108583743+morshed0099@users.noreply.github.com>
2024-05-26 19:16:27 +0600

    Delete dist directory

--- dist/app/midleware/globalErrorHandelar.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const globalErrorHandeler = (error, req, res, next) => {
-    let statusCode = error.statusCode || 500;
-    let message = error.message || 'something went wrong';
-    let errorDetails = error;
-};
-exports.default = globalErrorHandeler;
